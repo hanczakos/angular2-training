ca74580d88fd0ad1d1af20b495b1489059da6f23
Hancz <hanczakos@gmail.com>
2016-05-07 00:10:13 +0200

    Angular 2 project base  by @haszprus & @hanczakos

--- app/vendor.ts
+++ app/vendor.ts
@@ -3,3 +3,6 @@ import 'es6-shim';
 import 'es6-promise';
 import 'zone.js/dist/zone';
 import 'reflect-metadata';
+
+//Bootstrap css
+import 'bootstrap/dist/css/bootstrap.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -19,11 +20,16 @@ module.exports = {
         test: /\.ts/,
         loaders: ['ts-loader'],
         exclude: /node_modules/
-      }
+      },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js")
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"./dist/vendor.bundle.js"),
+    new ExtractTextPlugin("./dist/style.css", {
+      allChunks: true
+    })
   ],
   devServer: {
     contentBase: 'app/'
